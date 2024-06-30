0f50ff3f3b9f69ac308ef1acb7ac967d62830e92
muiruritrevor <trevormuirurii@gmail.com>
2024-06-30 21:31:54 +0300

    Add basic_operations.py with basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define variables with given values
+number1 = 10
+number2 = 5
+
+#perform arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+#print the result
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
