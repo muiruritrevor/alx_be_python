620365b3cbf600e40151531a65c51f1dbdc48a8e
muiruritrevor <trevormuirurii@gmail.com>
2024-07-21 22:14:31 +0300

    commit

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        amount = float(self.account_balance)
+        print(f"Current Balance: ${amount:.2f}")
